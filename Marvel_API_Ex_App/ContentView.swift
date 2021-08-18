60fcec53e8b0d4a0bbf7d393666ecf90b18cd97f
huongtravo0910 <huongtravo0910@gmail.com>
2021-08-18 21:15:13 +0700

    Initial Commit

---
+++ Marvel_API_Ex_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel_API_Ex_App
+//
+//  Created by Tra Vo on 18/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marvel_API_Ex_App/Marvel_API_Ex_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_API_Ex_AppApp.swift
+//  Marvel_API_Ex_App
+//
+//  Created by Tra Vo on 18/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_API_Ex_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
